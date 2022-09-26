8e7390050641d4c9cc687ab53aa5ec4b76a44a31
Khaled-AlKhateeb <khaled.w.khateeb85@gmail.com>
2022-09-26 16:12:20 +0300

    add modules

---
+++ modules/library.js
@@ -0,0 +1,13 @@
+class BookLibrary {
+  constructor() {
+    this.booksArr = [];
+    this.add = (Obj) => {
+      this.booksArr.push(Obj);
+    }
+    this.remove = (Obj) => {
+      this.booksArr.splice(Obj, 1);
+    }
+  }
+}
+
+export const library = new BookLibrary();
